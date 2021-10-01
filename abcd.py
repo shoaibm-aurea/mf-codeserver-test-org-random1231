1e9e4724a61ffed71475f23f8de5a7c75525c724
shoaibm-aurea <shoaib.mansoor@aurea.com>
2021-10-02 00:34:05 +0530

    asdasd

--- abcd.py
+++ abcd.py
@@ -1,23 +1,134 @@
-import requests
 import json
 import os
+
+import requests
+import random
+
 token = os.getenv('GITHUB_TOKEN')
+username = os.getenv('GIT_USERNAME')
+git_repo = os.getenv('GIT_REPO')
+DEFAULT_BRANCH = 'master'
 
-headers = {
-    "Authorization" : "token {}".format(token),
-    "Accept" : "application/vnd.github.sailor-v-preview+json"
-}
-data= {
-    "title" : "PullRequest-Using-GithubAPI",
-    "body" : "I have amazing new Features",
-    "head" : "pull-request",
-    "base" : "master"
-}
 
-username = input("Enter your Github username : ")
-Repositoryname = input("Enter the name of the repository : ")
+def create_blob():
+    url = "https://api.github.com/repos/{}/{}/git/blobs".format(username, git_repo)
+    headers = {
+        "Authorization": "token {}".format(token),
+        "Accept": "application/vnd.github.VERSION.raw"
+    }
 
-url = "https://api.github.com/repos/{}/{}/pulls".format(username,Repositoryname)
+    with open('abcd.py', 'r', encoding='utf-8') as f:
+        file_data = f.read()
 
-response = requests.post(url,data=json.dumps(data), headers=headers)
-print(response)
+    data = {
+        "content": file_data,
+        "encoding": "utf-8"
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print('** Blob created successfully **')
+    print(response.json()['sha'])
+    print(response.json()['url'])
+    return response.json()['sha']
+
+
+def create_tree(blob_sha, default_branch_sha):
+    url = "https://api.github.com/repos/{}/{}/git/trees".format(username, git_repo)
+    headers = {
+        "Authorization": "token {}".format(token),
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        # base_tree is needed to update the existing tree else all files are considered to be deleted with this tree
+        'base_tree': default_branch_sha,
+        'tree': [
+            {
+                'path': "abcd.py",
+                'mode': "100644",
+                'type': "blob",
+                'sha': blob_sha
+            }
+        ]
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print('** Tree created successfully **')
+    print(response.json()['sha'])
+    print(response.json()['url'])
+    return response.json()['sha']
+
+
+def create_commit(tree_sha, default_branch_sha):
+    url = "https://api.github.com/repos/{}/{}/git/commits".format(username, git_repo)
+    headers = {
+        "Authorization": "token {}".format(token),
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        'message': f"Random message: {random.random()}",
+        'tree': tree_sha,
+        'parents': [default_branch_sha]
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print(f'** Commit created successfully: {response.json()["sha"]}**')
+    print(response.json()['sha'])
+    print(response.json()['url'])
+    return response.json()['sha']
+
+
+def get_default_branch_head():
+    url = "https://api.github.com/repos/{}/{}/git/refs/heads".format(username, git_repo)
+    headers = {
+        "Authorization": "token {}".format(token),
+        "Accept": "application/vnd.github.v3+json"
+    }
+    response = requests.get(url, headers=headers)
+    print(f"** Branch heads pulled successfully: **")
+    data = response.json()
+    sha = next((i['object']['sha'] for i in data if i['ref'] == f'refs/heads/{DEFAULT_BRANCH}'), None)
+    return sha
+
+
+def create_branch(commit_sha):
+    url = "https://api.github.com/repos/{}/{}/git/refs".format(username, git_repo)
+    headers = {
+        "Authorization": "token {}".format(token),
+        "Accept": "application/vnd.github.v3+json"
+    }
+    branch_name = f"NEW-BRANCH-NAME-{int(random.random() * 1000)}"
+    data = {
+        "ref": f"refs/heads/{branch_name}",
+        "sha": commit_sha
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print(f"** Branch created successfully: {response.json()['object']['sha']} **")
+    print(f"** Branch name: {branch_name} **")
+    return branch_name
+
+
+def create_pull_request(branch_name):
+    url = "https://api.github.com/repos/{}/{}/pulls".format(username, git_repo)
+    headers = {
+        "Authorization": "token {}".format(token),
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        "title": "PullRequest-Using-GithubAPI-Dynamic",
+        "body": "I have amazing new Features",
+        "head": branch_name,
+        "base": "master"
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    return response.json()['url']
+
+
+if __name__ == '__main__':
+    blob_sha = create_blob()
+    default_branch_sha = get_default_branch_head()
+    tree_sha = create_tree(blob_sha, default_branch_sha)
+    commit_sha = create_commit(tree_sha, default_branch_sha)
+    branch_name = create_branch(commit_sha)
+    pr_url = create_pull_request(branch_name)
+    print(pr_url)
+
+
+    # for adding multiple commits, parent commit should be equal to the last commit sha
+
